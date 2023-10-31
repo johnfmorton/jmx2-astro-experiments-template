e402ece9122d41f8d0f636e6cc9ef39fd2e84572
John F Morton <morton@jmx2.com>
2023-10-31 10:26:26 -0400

    sample animation in place

---
+++ src/script/starter-script.ts
@@ -0,0 +1,29 @@
+import { gsap } from "gsap";
+
+import { CustomEase } from "gsap/CustomEase";
+
+/* The following eases are Club GSAP perks */
+import { CustomBounce } from "gsap/CustomBounce"; // extends CustomEase
+import { CustomWiggle } from "gsap/CustomWiggle"; // extends CustomEase
+
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+/* The following plugins are Club GSAP perks */
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { GSDevTools } from "gsap/GSDevTools";
+import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
+
+
+gsap.registerPlugin(Flip, ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin, DrawSVGPlugin, ScrollSmoother, GSDevTools, InertiaPlugin, MorphSVGPlugin, MotionPathHelper, Physics2DPlugin, PhysicsPropsPlugin, CustomEase, CustomBounce, CustomWiggle);
+
+console.log("gsap", gsap);
